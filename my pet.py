af53a758fce4c5142d4d7bdfa789a670dfec816a
Santhucurbi <134621914+Santhucurbi@users.noreply.github.com>
2023-05-25 23:29:51 +0530

    Add files via upload

---
+++ my pet.py
@@ -0,0 +1,8 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==== RESTART: C:/Users/Abans/AppData/Local/Programs/Python/Python311/mp46.py ===
+>>> 
+======== RESTART: C:/Users/Abans/Desktop/python projects/colour rings.py =======
+
+=========== RESTART: C:/Users/Abans/Desktop/python projects/my pet.py ==========
